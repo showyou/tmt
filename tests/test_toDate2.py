d656cd9171eaaf7b90336ccef7b63c06a29b66dc
show you <showyou41@gmail.com>
2010-06-11 23:55:46 +0900

    todate2

---
+++ tests/test_toDate2.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.dirname(__file__)).rsplit("/",1)[0],
+)
+
+def test_toDate():
+    import datetime
+    import toDate2
+    date = "Sat Dec 22 01:07:16 +0000 2007"
+    date2 = toDate2.toDate(date)
+    if str(date2).startswith("2007-12-22 10:07:16"):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    import nose
+    nose.main()
+
