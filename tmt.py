899f481ae7891f2c574fccb7e8acadbd70f9747f
showyou <showyou41@gmail.com>
2010-06-18 08:14:27 +0900

    名前変えた

--- tmt.py
+++ tmt.py
@@ -15,7 +15,7 @@ def sendTmt(userName,toMail):
     latestTime = datetime.datetime.today()-datetime.timedelta(days=1)
     pageNum = 50
     
-    header = u"  :: Today's my twitter ::\n"+unicode(getUser,"utf-8")+u"さんの一日の発言です。\n\n"
+    header = u"  :: Today's my tweets ::\n"+unicode(getUser,"utf-8")+u"さんの一日の発言です。\n\n"
     header = header.encode("iso-2022-jp")
     outSentence = ""
     try:
@@ -35,14 +35,14 @@ def sendTmt(userName,toMail):
         traceback.print_exc()
     
     outSentence = header + outSentence
-    outSentence += "\n  Today's my twitter by showyou(twitter.com/showyou)\n"
+    outSentence += "\n  Today's my tweets by showyou(twitter.com/showyou)\n"
     #print unicode(outSentence,"iso-2022-jp").encode("cp932")
     userdata2 = jsonfile.read(homePath + "/user/twdata_tmt")
     user = userdata2["user"]
     passWord = userdata2["pass"]
     from_addr = user
     to_addr = toMail
-    title = "today's my twitter:"+latestTime.strftime('%Y/%m/%d')
+    title = "today's my tweets:"+latestTime.strftime('%Y/%m/%d')
     title = title.encode("iso-2022-jp","ignore")
     msg = mail.create_message2(from_addr, to_addr, title, outSentence, 'ISO-2022-JP')
     mail.send_via_gmail(user,passWord,from_addr, to_addr, msg)
